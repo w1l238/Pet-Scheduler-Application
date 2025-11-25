02235b30d960d69d025a6b1a2c917f6d09e6033b
w1l238 <wdooley15@outlook.com>
2025-11-25 14:36:25 -0500

    Manage services page.

---
+++ frontend/src/pages/ManageServicesPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ServiceManager from "../components/ServiceManager";
+
+function ManageServicesPage() {
+    return (
+        <ServiceManager />
+    );
+}
+
+export default ManageServicesPage;
