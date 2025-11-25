9ad5d55cfcd09ca5250a33c6ed10ca3efa77e53a
w1l238 <wdooley15@outlook.com>
2025-11-25 14:34:22 -0500

    All invoices page.

---
+++ frontend/src/pages/AllInvoicesPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import InvoiceManager from "../components/InvoiceManager";
+
+function AllInvoicesPage() {
+    return (
+        <InvoiceManager />
+    );
+}
+
+export default AllInvoicesPage;
