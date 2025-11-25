e770724d6c70efc87d1d624ca3df63fd9f94a9d8
w1l238 <wdooley15@outlook.com>
2025-11-25 14:34:11 -0500

    All appointments page.

---
+++ frontend/src/pages/AllAppointmentsPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AllAppointments from "../components/AllAppointments";
+
+function AllAppointmentsPage() {
+    return (
+        <AllAppointments />
+    );
+}
+
+export default AllAppointmentsPage;
