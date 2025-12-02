2a4e86577889d7e73399a24ff7f37e53334be5f4
w1l238 <wdooley15@outlook.com>
2025-12-01 23:11:46 -0500

    Added line to run docker container when I get to it.

--- backend/db.js
+++ backend/db.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
+    // Use this for running server.js locally
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+
+    // This line is for the docker container to use enviornment variables defined in a compose file.
+    //connectionString: process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`,
+
 });
 
 module.exports = pool;
