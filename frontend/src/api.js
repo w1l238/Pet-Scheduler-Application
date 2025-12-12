d68a6d8ebb5b0020274ce201dd590e3548f0574c
w1l238 <wdooley15@outlook.com>
2025-12-12 00:43:00 -0500

    Allow 401 error for login page to display the toast notification on bad login.

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -22,7 +22,7 @@ api.interceptors.request.use(
 api.interceptors.response.use(
     response => response,
     error => {
-        if (error.response && error.response.status === 401) {
+        if (error.response && error.response.status === 401 && error.config.url !== '/auth/login') {
             // For example, redirect to login or refresh token
             localStorage.removeItem('token');
             window.location.href = '/login';
